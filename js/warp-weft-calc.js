35141c246ac3650775987cf91e4c4d01ae1f5594
Fiona Voss <fionavoss@gmail.com>
2016-06-16 16:12:43 -0700

    First commit

---
+++ js/warp-weft-calc.js
@@ -0,0 +1,15 @@
+// Document ready function
+$(document).ready(function() {
+
+  //when the submit button is clicked
+  $("#submit").on(
+    "click",
+    function() {
+      alert("Button was clicked");
+    });
+
+  // calculates length of each warp thread
+  // warp_length = woven_length * take-up + loom waste + sampling
+  
+
+});
