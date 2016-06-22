caa509d4715dc39ee4136e089a06f71eaebaf951
learn <learn@learn06.local>
2016-06-21 18:21:12 -0700

    calculates warp length

--- js/warp-weft-calc.js
+++ js/warp-weft-calc.js
@@ -1,3 +1,11 @@
+// calculates length of each warp thread
+// warp_length = woven_length * take-up + loom waste + sampling
+// calculateWarpLength(10, 10, 24, 10) --> 45
+// calculateWarpLength(10, 10, 10, 10) --> 31
+function calculateWarpLength(wovenLength, takeUp, loomWaste, sampling) {
+  return(wovenLength + (wovenLength * takeUp / 100) + loomWaste + sampling);
+}
+
 // Document ready function
 $(document).ready(function() {
 
@@ -5,11 +13,13 @@ $(document).ready(function() {
   $("#submit").on(
     "click",
     function() {
-      alert("Button was clicked");
+      var wovenLength = parseInt($("#wovenLength").val());
+      var warpTakeUp = parseInt($("#warpTakeUp").val());
+      var loomWaste = parseInt($("#loomWaste").val());
+      var sampling = parseInt($("#sampling").val());
+      var warpLength = calculateWarpLength(wovenLength, warpTakeUp, loomWaste, sampling);
+      $("#warpLength").innerHTML(warpLength);
     });
 
-  // calculates length of each warp thread
-  // warp_length = woven_length * take-up + loom waste + sampling
-  
 
 });
